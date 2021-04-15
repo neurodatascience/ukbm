250d044b5799cc0a32bab1653f999effa81efc9d
Alexandre Hutton <50920802+AlexandreHutton@users.noreply.github.com>
2021-04-15 10:25:34 -0400

    added conditional overlay for files that need to be fixed; fixed pathing, sed, overlay

--- bashtools/remove_subject.sh
+++ bashtools/remove_subject.sh
@@ -7,6 +7,7 @@ where:
   -d DIRECTORY                  Directory to squash. (typically directory in /)
   -o OUTPUT_DIR                 Directory where to save resquashed images
   -a ACCOUNT                    Account to use for SLURM accounting.
+  -j, --json DATASET_DESC.json	Replacement for dataset_description.json.
   subject_list                  Text file with subject directories to remove.
   singularity_img               Singularity image to use to mount SquashFS files.
   squashfs                      SquashFS files to mount, check, and resquash without the subjects to be removed.
@@ -34,6 +35,10 @@ while (( "$#" )); do
       account="$2"
       shift 2
       ;;
+    -j|--json)
+      dataset_desc="${2}"
+      shift 2
+      ;;
     -*|--*)
       echo "Unrecognized option: ${1}"
       exit 1
@@ -59,8 +64,6 @@ squash_to_fix=()
 for squash in "${squash_list[@]}"; do
   echo "Checking ${squash}..."
   buf=`singularity exec -B ${script_dir}:/fix_scripts/ -B ${subject_list_dir}:/.subjects/ --overlay "${squash}":ro "${singularity_img}" bash /fix_scripts/check_subject.sh /.subjects/${subject_list}`
-  echo "Checked ${squash}"
-  echo "buf: ${buf}"
   if [ "${buf}" -eq 1 ]; then
     squash_to_fix+=(${squash})
     echo "Needs fixing: ${squash}"
@@ -69,5 +72,10 @@ for squash in "${squash_list[@]}"; do
 done
 # subject_list singularity_img output_dir topdir squashfs_0
 for squash in "${squash_to_fix[@]}"; do
-  sbatch --account=${account} ${script_dir}/resquash.sh  ${subject_list} ${singularity_img} ${output} ${squash_dir} ${squash}
+  echo "Submitting ${squash} ..."
+  if [ -n "${dataset_desc}" ]; then
+    sbatch --account=${account} -J "resquash" ${script_dir}/resquash.sh -p -j ${dataset_desc} ${subject_list} ${singularity_img} ${output} ${squash_dir} ${squash}
+  else
+    sbatch --account=${account} -J "resquash" ${script_dir}/resquash.sh -p  ${subject_list} ${subject_list} ${singularity_img} ${output} ${squash_dir} ${squash}
+  fi
 done
--- bashtools/resquash.sh
+++ bashtools/resquash.sh
@@ -2,7 +2,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu=8G
-#SBATCH --time=0-12:00:00
+#SBATCH --time=1-00:00:00
 
 helpstr="$(basename "$0") [-h] [-p] [-j dataset_description.json] subject_list singularity_img output_dir topdir squashfs - Re-squashes the input SquashFS files without the subjects listed
 
@@ -52,6 +52,7 @@ ST=${SLURM_TMPDIR}
 
 # Find safe output
 raw_output_name=`basename ${squash_file}`
+output_name=${raw_output_name}
 ind=0
 while [ -f ${output_dir}/${output_name} ]; do
   output_name=${raw_output_name%%.*}_${ind}.${output_name#*.}
@@ -69,15 +70,20 @@ if [ ${part_dir:0:1} == "/" ]; then
     part_dir=${part_dir:1}
 fi
 mkdir -p ${ST}/${part_dir}
+chmod -R o+rx ${ST}/${part_dir}
 
 if [ "${fix_participants}" -eq 1 ]; then
   # extract participants.tsv; store in same path structure for squashing
-  singularity exec --overlay ${squash_file}:ro -B ${ST}/${part_dir}:/.st_tmp/ cp ${part_path} /.st_tmp/
-  # remove subjects
-  while read -r exc; do
-    exc_base=`basename ${exc}`
-    sed -i '/${exc_base}/d' ${ST}/${part_dir}/participants.tsv
-  done < ${exclude_file}
+  singularity exec --overlay ${squash_file}:ro -B ${ST}/${part_dir}:/.st_tmp/ ${singularity_image} cp ${part_path} /.st_tmp/
+  # remove subjects from participants.tsv
+  if [ -f "${ST}/${part_dir}/participants.tsv" ]; then
+    while read -r exc; do
+      exc_base=`basename ${exc}`
+      sed -i "/${exc_base}/d" ${ST}/${part_dir}/participants.tsv
+    done < ${exclude_file}
+  else
+    fix_participants=0
+  fi
 fi
 
 # Check for updating dataset_description
@@ -89,16 +95,20 @@ fi
 # If we need to fix either dataset or participants, create new squash for overlay
 part_dir_top=${part_dir%%/*}
 if [ "${fix_participants}" -eq 1 ] || [ -n "${dataset_description}" ]; then
-  squash_command="mksquashfs /to_squash/${part_dir_top} ${ST}/txtfix.squashfs -no-progress -noI -noD -noX -keep-as-directory -all-root -processors 1 -no-duplicates -fstime 1601265600"
+  squash_command="mksquashfs /to_squash/${part_dir_top} /to_squash/txtfix.squashfs -no-progress -noI -noD -noX -keep-as-directory -all-root -processors 1 -no-duplicates -fstime 1601265600"
+  chmod -R o+rx ${ST}/${part_dir_top}
   singularity exec -B ${ST}:/to_squash/ ${singularity_image} ${squash_command}
 fi
 
 exclude_filename=`basename ${exclude_file}`
 exclude_dir=`dirname ${exclude_file}`
 if [ -f "${ST}/txtfix.squashfs" ]; then
-  overlay="--overlay ${squash_file}:ro --overlay ${ST}/txtfix.squashfs"
+  overlay="--overlay ${squash_file}:ro --overlay ${ST}/txtfix.squashfs:ro"
 else
   overlay="--overlay ${squash_file}:ro"
 fi
+cp ${exclude_file} ${ST}/${exclude_filename}
+
 squash_command="mksquashfs ${topdir} /.out/${output_name} -ef /.exclude/${exclude_filename} -no-progress -noI -noD -noX -keep-as-directory -all-root -processors 1 -no-duplicates -fstime 1601265600"
-singularity exec ${overlay} -B ${exclude_dir}:/.exclude/ -B ${output_dir}:/.out/ ${singularity_image} ${squash_command}
+singularity exec ${overlay} -B ${ST}:/.exclude/ -B ${output_dir}:/.out/ ${singularity_image} ${squash_command}
+
