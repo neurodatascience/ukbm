9d8f8c85944891679697046fe787fe1892586f40
Alexandre Hutton <50920802+AlexandreHutton@users.noreply.github.com>
2021-04-20 11:43:19 -0400

    added slurm squashing; added parallel fetching for bulk data

---
+++ slurm/squashdir.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+#SBATCH --array=0
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --time=1-00:00:00
+
+helpstr="$(basename $0) [-h] [-d depth] [-f format] [-n numim] directory_to_squash
+Squashes the specified directory. The number of output SquashFS files is dictated by the number of jobs in the submitted job array (--array to sbatch).
+
+where:
+  -h                            Show this message.
+  -d, --depth DEPTH		Depth of directory to use for splitting. Default 0. E.g. /neurohub/ukbb/imaging/sub-* would require -d 3.
+  -f, --format FORMAT		Formatting string to use for naming output. It should change with ${SLURM_ARRAY_TASK_ID} to prevent collisions. Default: 'neurohub_ukbb_data_${SLURM_ARRAY_TASK_ID}'. Recommended: 'neurohub_ukbb_MODALITY_sesSESSION_${SLURM_ARRAY_TASK_ID}_STRUCTURE.squashfs'.
+				NOTE: input should be specified using single quotes to avoid variable expansion.
+  -n, --numim NUMIM		Number of images that would be produced. Default to ${SLURM_ARRAY_TASK_COUNT}. Only used when a subset needs to be re-squashed. A job that was previously-submitted with --array=0-3 would produce 4 images. If you need to re-squash image 2, you would use: sbatch --array=2 [...] $(basename $0) -n 4 [...].
+  --dry				If set, will not squash but will instead print the list of files that would be excluded.
+  directory_to_squash		Directory containing the data that needs to be squashed.
+
+Example:
+sbatch --account=rpp-account-aa --array=0-5 $(basename $0) -d 3 -f 'neurohub_ukbb_rfmri_ses2_${SLURM_ARRAY_TASK_ID}_bids.squashfs' data/
+Resquashing images 2,3:
+sbatch --account=rpp-account-aa --array=2-3 $(basename $0) -d 3 -f 'neurohub_ukbb_rfmri_ses2_${SLURM_ARRAY_TASK_ID}_bids.squashfs' -n 6 data/
+"
+
+### Parse input
+nameformat='neurohub_ukbb_data_${SLURM_ARRAY_TASK_ID}'
+splitdepth=0
+numim="${SLURM_ARRAY_TASK_COUNT}"
+pos_args=()
+while (( "$#" )); do
+  case "$1" in
+    -h|--help)
+      echo $helpstr
+      exit 0
+      ;;
+    -d|--depth)
+      splitdepth="${2}"
+      shift 2
+      ;;
+    -f|--format)
+      nameformat="${2}"
+      shift 2
+      ;;
+    -n|--numim)
+      numim="${2}"
+      shift 2
+      ;;
+    --dry)
+      SLURM_TEST_ONLY=1
+      shift 1
+      ;;
+    -*|--*)
+      >&2 echo "Unrecognized option ${1}"
+      exit 1
+      ;;
+    *)
+      pos_args+=("$1")
+      shift 1
+      ;;
+  esac
+done
+
+squashdir=${pos_args[0]}
+echo "squashdir: ${squashdir}"
+### Input parsed
+ST=${SLURM_TMPDIR}
+
+# Get full path
+squashdir=`readlink -f "${squashdir}"`
+
+# Need to create exclusion list for mksquashfs
+# Use splitdepth to limit splitting depth
+IFS=$'\n' eval 'dirlist=(`find "${squashdir}" -maxdepth ${splitdepth} -mindepth ${splitdepth} | sort`)'
+# Split list into even pieces
+ndirs=${#dirlist[@]}
+nperjob=$((ndirs/numim+1))  # +1 because of bash's flooring
+startind=$((SLURM_ARRAY_TASK_ID*nperjob))
+#include_list=(${dirlist[@]:startind:nperjob})
+
+# Generate exclude list; add all directories we previously found except for a range
+exclude_list=()
+exclude_list+=(${dirlist[@]:0:startind})
+exclude_list+=(${dirlist[@]:startind+nperjob})
+tdir=`mktemp -d -p ${ST} tmp.XXXXXX`
+printf "%s\n" ${exclude_list[@]} > ${tdir}/exclude.txt
+outname="`eval echo $nameformat`"
+
+if [ -z "${SLURM_TEST_ONLY}" ]; then
+	SLURM_TEST_ONLY=0
+fi
+
+echo "Total number of files/dirs: ${#dirlist[@]}"
+echo "Number in exclude list: ${#exclude_list[@]}"
+echo "Difference: $((${#dirlist[@]}-${#include_list[@]}-${#exclude_list[@]}))"
+echo "Outname: ${outname}"
+if [ "${SLURM_TEST_ONLY}" -eq 1 ]; then
+	printf "%s\n" ${exclude_list[@]} > ./exclude_${outname}.txt
+	exit 0
+fi
+
+mksquashfs ${squashdir} ${outname} -ef ${tdir}/exclude.txt -no-progress -noI -noD -noX -keep-as-directory -all-root -processors 1 -no-duplicates -fstime 1601265600
+
---
+++ transfertools/parallel_fetch.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+# This code is intended to facillitate downloading bulk UKB data.
+# Need: 1) fetch utility, 2) bulkfile 3) keyfile 4) array idx 5) num jobs
+
+helpstr="$(basename $0) [-h] [-n numjobs] [-f field_id] [-b bulkfile] [-p blocklist] [-s] fetch_utility keyfile
+
+where:
+  -h                            Show this message.
+  -n, --numjobs NUM		Number of parallel downloads. Default 10.
+  -f, --field FIELD_ID		Field ID to download. If unspecified, download all available data from bulkfile.
+  -b, --bulkfile BULK		File obtained from ukbconv [...] bulk. Required if fetch_utility is ukbfetch
+  -p, --blocklist BLOCK		Block list info, 'pvcf_blocks.txt'. Required for Data-Field 23156.
+  -s, --skip			If set, skip file if already present at output. Default 0.
+  fetch_utility			Path to either ukbfetch (bulk imaging) or gfetch (bulk genetics)
+  keyfile			Path to authentication key provided by the UKB.
+
+Example use:
+bash $(basename $0) -f 20250_2_0 -b dataset.bulk -s ukbfetch k123r567.key
+"
+
+# Parse inputs
+skipflag=0
+njob=10
+pos_args=()
+while (( "$#" )); do
+        case "$1" in
+		-n|--numjobs)
+		  # number of jobs downloading the data (used to determine how much each should download)
+		  njob="${2}"
+		  shift 2
+		  ;;
+		-f|--field)
+		  # field id
+		  field="${2}"
+		  shift 2
+		  ;;
+		-b|--bulkfile)
+		  # bulk file; used with ukbfetch instead of gfetch (same structure!)
+		  bulkfile="${2}"
+		  shift 2
+		  ;;
+		-p|--blocklist)
+		  # pvcf_blocks.txt
+		  blocklist="${2}"
+		  shift 2
+		  ;;
+		-s|--skip)
+		  # if 1: skip if file is already present; default 1
+		  skipflag=1
+		  shift 1
+		  ;;
+		-*|--*)
+                  >&2 echo "Invalid flag ${2}"
+                  exit 1
+                  ;;
+		*)
+		  pos_args+=("${1}")
+		  shift 1
+		  ;;
+        esac
+done
+
+fetch=`readlink -f ${pos_args[0]}`
+keyfile=${pos_args[1]}
+
+
+echo "Checking input values..."
+# Check that all necessary variables are defined
+if [ -z "${fetch}" ]; then
+	>&2 echo "Fetch utility undefined"
+	exit 1
+fi
+if [ -z "${keyfile}" ]; then
+	>&2 echo "Keyfile undefined."
+	exit 1
+fi
+
+# Define download unit
+dload () {
+local chrom
+chrom="${1}"
+${fetch} ${field} -c${chrom} -a${keyfile}
+${fetch} ${field} -c${chrom} -m -a${keyfile}
+}
+
+# Define download unit (with blocks!)
+dload_block () {
+local chrom
+local block
+chrom="${1}"
+block="${2}"
+${fetch} ${field} -c${chrom} -b${block} -a${keyfile}
+}
+
+dload_bulk () {
+local startind
+local maxfiles
+startind="${1}"
+maxfiles=1000
+${fetch} -b${bulkfile} -a${keyfile} -s${startind} -m${maxfiles}
+}
+
+chromlist=(`seq 1 22`)
+chromlist+=(X Y XY MT)
+
+job_ind=0
+echo "Downloading!"
+if [ ! -z ${blocklist} ]; then
+	# Go through each line in blocklist to get chrom-block pair
+	# blocklist specified; gfetch & blockfile parse
+	echo "Parsing blockfile ${blockfile}!"
+	while read -r binfo; do
+		barr=(${binfo})
+		chrom=${barr[1]}
+		block=${barr[2]}
+		if [ ${skip} -eq 1 ]; then
+			if [ -f "ukb${field}_c${chrom}_b${block}_v1.vcf.gz" ]; then
+				echo "Found file; skipping: ukb${field}_c${chrom}_b${block}_v1.vcf.gz"
+				continue
+			fi
+		fi
+		dload_block ${chrom} ${block} &
+		job_ind=$((job_ind+1))
+		if [ ${job_ind} -ge ${njob} ]; then
+			wait
+			job_ind=0
+		fi
+	done < ${blocklist}
+elif [ ! -z ${bulkfile} ]; then
+	# bulkfile is specified; use ukbfetch
+	echo "Parsing bulkfile ${bulkfile}!"
+	maxfiles=1000
+	numlines=`cat ${bulkfile} | wc -l`  # number of specified files
+	numsub=$((1+numlines/maxfiles))
+	job_ind=0
+	for i in `seq 0 ${numsub}`; do
+		startind=$((maxfiles*i))
+		echo "Startind: ${startind}"
+		dload_bulk ${startind} &
+		job_ind=$((job_ind+1))
+		if [ ${job_ind} -ge ${njob} ]; then
+			wait
+			job_ind=0
+		fi
+	done
+elif [ -z ${blocklist} ]; then
+	echo "Downloading genetics data!"
+	for chrom in ${chromlist[@]}; do
+		dload ${chrom} &
+		job_ind=$((job_ind+1))
+		if [ ${job_ind} -ge ${njob} ]; then
+			wait
+			job_ind=0
+		fi
+	done
+fi
+
