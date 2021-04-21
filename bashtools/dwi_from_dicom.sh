3858aa8bcadb348078d147a620d12d37bdf88904
Lex <50920802+AlexandreHutton@users.noreply.github.com>
2021-04-21 10:56:31 -0400

    changed acquisition direction detection

--- bashtools/dwi_from_dicom.sh
+++ bashtools/dwi_from_dicom.sh
@@ -2,13 +2,14 @@
 
 # This code converts UKB DWI dicomes into BIDS
 
-helpstr="$(basename "$0") [-h] [-s skip_flag] zipfilelist bidsdir - Code to convert UKB diffusion DICOMs into BIDS.
+helpstr="$(basename "$0") [-h] [-s skip_flag] [-p permission] zipfilelist bidsdir - Code to convert UKB diffusion DICOMs into BIDS.
 
 where:
-  -h            Show this message.
-  -s            Whether to skip a subject if present in output directory (0=don't skip, 1=skip). Defaults to 1.
-  zipfilelist   Text file with newline-delimited list of zipped DWI DICOMs.
-  bidsdir       Output directory.
+  -h              Show this message.
+  -s              Whether to skip a subject if present in output directory (0=don't skip, 1=skip). Defaults to 1.
+  -p, --perm PERM Set permission of output file to PERM. E.g.: 700, o+rx, 744.
+  zipfilelist     Text file with newline-delimited list of zipped DWI DICOMs.
+  bidsdir         Output directory.
 "
 
 
@@ -34,6 +35,10 @@ while (( "$#" )); do
       ;;
     -s|--skip)
       skip_flag="${2}"
+      shift 2
+      ;;
+    -p|--perm)
+      perm="${2}"
       shift 2
       ;;
     -*|--*)
@@ -91,8 +96,15 @@ while read -r fil; do
       conv_base=${bv%.bval}
       seqbase=`basename ${conv_base}`
       seqsplit=(${seqbase//_/ })
-      acq_dir=${seqsplit[1]}
-
+      # get acquisition dir
+      if [[ "${seqbase}" =~ .*"_AP_".* ]] && [[ ! "${seqbase}" =~ .*"_PA_".* ]]; then
+        acq_dir="AP"
+      elif [[ ! "${seqbase}" =~ .*"_AP_".* ]] && [[ "${seqbase}" =~ .*"_PA_".* ]]; then
+        acq_dir="PA"
+      else
+        >&2 echo "Error with ${zipname}; acquisition direction not detected. Skipping"
+        break
+      fi
       bids_pref="sub-${subid}_ses-${session}_acq-${acq_dir}_dwi"
       for conv in $(printf "%s\n" "${conv_base}*"); do
         # get suffix
@@ -100,9 +112,11 @@ while read -r fil; do
         suff=${convbasename#*.}
         # check device for mv vs. cp
         mv ${conv} ${bids_dir}/${bids_pref}.${suff}
-
       done
     fi
   done
+  if [ -n "${perm}" ]; then
+    chmod ${perm} ${bids_dir}/*
+  fi
   rm -r ${nii_tmp} &
 done < ${file_list}
